b7b43c8c8c81f99ab296abd68419476e73345a3a
vikram745 <77730212+vikram745@users.noreply.github.com>
2021-02-21 20:45:05 +0530

    inital commit

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php>
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message']
+
+
+    $email_form = 'vikramgoswami745@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n";
+                    "User Email: $visitor_email.\n";
+                        "User Message: $message.\n";
+
+
+    $to = "gpssmalsar1@gmail.com";
+
+    $headers = "Form: $email_form\r\n";
+
+    $headers = "Reply-To: $visitor_email\r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+?>
